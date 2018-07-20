6ecfd3f658ad71c53e3c4baf7006ddc1c1f87351
Lee Young Seo <lupin204@gmail.com>
2018-07-20 21:32:04 +0900

    test main..

---
+++ main.js
@@ -0,0 +1,60 @@
+const electron = require("electron"),
+    path = require("path"),
+    url = require("url");
+
+/*
+const app = electron.app;                           // node.js의 express app 같은 초기화.
+const BrowserWindow = electron.BrowserWindow;       // <브라우저 window> 타입 설정 = BrowserWindow:웹사이트처럼보임
+*/
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+// application 시작 함수
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        resizable: false,
+        title: "Lupincoin Wallet"
+    });
+
+    const ENV = process.env.ENV;
+    console.log(ENV);
+
+    // 개발기에서는 reactApp을 localhost를 바라보게 하고, 운영에서는 build된 곳을 보게 함.
+    if (ENV === "dev") {
+        mainWindow.loadURL("http://localhost:3000");
+    } else {
+        mainWindow.loadURL(
+            url.format({
+                pathname: path.join(__dirname, "uidev/build/index.html"),
+                protocol: "file",
+                slashes: true
+            })
+        );
+    }
+
+
+
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+
+}
+
+// MacOS에서는 elctron App을 닫아도 App이 실제로는 종료되지 않음.
+// --> 탭에서 클릭하면 다시 App을 띄우도록 처리.
+app.on("window-all-closed", () => {
+    // Darwin = Apple에서 만든 OS(리눅스 커널같은 존재)
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+})
+app.on("activate", () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+})
+
+app.on("ready", createWindow);
